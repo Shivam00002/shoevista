a1bb0ee32ed75787188cbf424259fc7676a7ff38
Shivam00002 <dubey02shiv@gmail.com>
2024-02-29 23:51:50 +0530

    added typeScript

--- src/components/checkout/Checkout.tsx
+++ src/components/checkout/Checkout.tsx
@@ -8,15 +8,13 @@ import { PlateformImages } from "../home/data/ImgData";
 export const Checkout = () => {
   return (
     <>
-
-    
-      <div className="md:w-full mx-auto w-full h-full px-2 py-2  md:flex  gap-5">
+      <div className="md:w-full mx-auto relative w-full h-full px-2 border border-red-600 py-2  md:flex  gap-5">
         <div className="md:w-full w-full h-full ">
           <h1 className="md:text-[45px] text-[25px] text-green-900">
             HAY Orange and white chairs
           </h1>
           <div className="md:flex md:mt-5 mt-3 border-b-2 pb-5 gap-4">
-            <div className="md:w-[485px]   w-[100%]  cursor-pointer h-[350px] md:h-[500px] relative rounded-sm">
+            <div className="md:w-[385px]   w-[100%]  cursor-pointer h-[350px] md:h-[400px] relative rounded-sm">
               <Image
                 src="https://www.relievefurniture.com/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Frelieve-image-cloud-store%2Fimage%2Fupload%2Fv1695143809%2Fitem-images%2F6ea064a7-e924-4c31-9e2a-5667f1339e3b.jpg&w=828&q=75"
                 alt="product-img"
@@ -24,7 +22,7 @@ export const Checkout = () => {
                 className="object-cover rounded-sm"
               />
             </div>
-            <div className="md:w-[485px]   w-[100%]  cursor-pointer h-[350px] md:h-[500px] relative rounded-sm">
+            <div className="md:w-[385px]   w-[100%]  cursor-pointer h-[350px] md:h-[400px] relative rounded-sm">
               <Image
                 src="https://www.relievefurniture.com/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Frelieve-image-cloud-store%2Fimage%2Fupload%2Fv1695143809%2Fitem-images%2F6ea064a7-e924-4c31-9e2a-5667f1339e3b.jpg&w=828&q=75"
                 alt="product-img"
@@ -33,19 +31,19 @@ export const Checkout = () => {
               />
             </div>
           </div>
-          <div className="md:flex hidden items-center justify-around">
+          {/* <div className="md:flex hidden items-center justify-around">
             <p className="text-[14px] text-green-900">MATERIAL</p>
             <div>
               <p className="text-[14px] text-green-900">COLOR</p>
               <p className="text-[14px] text-green-900">White</p>
             </div>
-          </div>
+          </div> */}
 
-          <div className=" grid grid-cols-2 md:grid-cols-4 mt-7  gap-2">
+          {/* <div className=" grid grid-cols-2 md:grid-cols-4 mt-7  gap-2">
             {[...new Array(4)].map((el, index) => {
               return <SpecificationBox key={index} />;
             })}
-          </div>
+          </div> */}
 
           <div className="md:mt-10 w-ful hit-fit md:py-6 py-3 px-2  border-b-2">
             <p className="text-[14px] text-gray-500 mt-5">DESCRIPTION</p>
@@ -69,12 +67,12 @@ export const Checkout = () => {
           </div>
         </div>
 
-        <div className="md:w-[30%] block w-full h-fit  md:h-screen ">
+        <div className="md:w-fit  w-full h-fit  md:h-screen ">
           <CheckBox />
         </div>
       </div>
 
-      <div className=" md:w-full mx-auto w-full  grid md:grid-cols-4 grid-cols-2 mt-5 border gap-3">
+      {/* <div className=" md:w-full mx-auto w-full  grid md:grid-cols-4 grid-cols-2 mt-5 border gap-3">
         {PlateformImages?.map((el, index) => {
           return (
             <PlatformBox
@@ -84,7 +82,7 @@ export const Checkout = () => {
             />
           );
         })}
-      </div>
+      </div> */}
     </>
   );
 };
