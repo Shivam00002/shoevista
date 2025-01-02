7f58a63690b9e9d31392341ab632eb5901bd9340
Shivam Dubey <108024596+Shivam00002@users.noreply.github.com>
2025-01-02 23:43:36 +0530

    Update Filter.tsx

--- src/components/filter/Filter.tsx
+++ src/components/filter/Filter.tsx
@@ -4,7 +4,7 @@ import { InputBox } from "./InputBox";
 export const Filter = () => {
    const [data , setData] = useState(true)
   
-  
+
   return (
     <div className="mt-20 px-2">
       <h2 className="font-semibold text-gray-500">Color</h2>
