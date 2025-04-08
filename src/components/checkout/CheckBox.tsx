0481a168efc0f7dbb270d148c93d3b327cd71838
Shivam Dubey <108024596+Shivam00002@users.noreply.github.com>
2025-04-08 23:47:47 +0530

    Update CheckBox.tsx

--- src/components/checkout/CheckBox.tsx
+++ src/components/checkout/CheckBox.tsx
@@ -22,7 +22,6 @@ export const CheckBox = ({ price }: any) => {
     }
   };
 
-
   return (
     <div className="md:w-[350px] px-2 py-2 w-full sticky  top-40 shadow-lg  h-fit md:h-[500px] rounded-md  ">
       <div className="w-full h-14 px-4 bg-[#C3FF52] flex justify-between items-center ">
